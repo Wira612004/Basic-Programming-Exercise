0ec11b9bc7e3a4c37579f06c2ffaee874bbab3ad
Wira612004 <114727898+Wira612004@users.noreply.github.com>
2022-10-01 18:39:20 +0700

    Add files via upload
    This is the Real Project

---
+++ age-calculator-v2.py
@@ -0,0 +1,24 @@
+# Program created By Wira Harsa
+# Majoring Informatics Engineering
+# TI22i class
+
+# Get your Birth Year
+birth_year = input("Enter Your Birth year! ")
+
+# Print Birth year
+print (birth_year)
+
+# Get current year
+current_year = input("Enter current year! ")
+
+# Print current year
+print (current_year)
+
+# Calculating Your age
+birth_year = int(birth_year)
+current_year = int(current_year)
+
+age = current_year - birth_year
+
+# The print of the result
+print ("your age is " + str(age))
