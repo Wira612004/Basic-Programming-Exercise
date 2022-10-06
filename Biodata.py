16e1bf1e0bcca5c6aec4129413094946340ae0e0
Wira612004 <114727898+Wira612004@users.noreply.github.com>
2022-10-06 11:44:06 +0700

    Biodata by Python Programming Language

---
+++ Biodata.py
@@ -0,0 +1,19 @@
+# This program was created by Me :3
+# Wira Harsa Abiyasa Rifki Ghani
+
+# Input Section
+print("                     --Please complete this data below--")
+nama = input("Enter your name          : ")
+prod = input("Enter your study program : ")
+kls = input("Enter your class         : ")
+hob = input("What is your hobby?      : ")
+qut = input("What is your motto?      : ")
+print("\n")
+
+# Data that will show after input section
+print("                     --Your Biodata--")
+print("Name          : ", nama)
+print("Study program : ", prod)
+print("Class         : ", kls)
+print("Hobby         : ", hob)
+print("Motto         : ", qut)
