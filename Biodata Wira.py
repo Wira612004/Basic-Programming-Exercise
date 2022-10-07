3db172781c4ff823b6e9c3117d5a426d7b2aee1f
Wira612004 <114727898+Wira612004@users.noreply.github.com>
2022-10-07 15:48:02 +0700

    Biodata Input

---
+++ Biodata Wira.py
@@ -0,0 +1,27 @@
+# This program was created by Me :3
+# Wira Harsa Abiyasa Rifki Ghani
+
+# Input Section
+print("                     --Please complete this data below--")
+nama = input("Enter your name          : ")
+prod = input("Enter your study program : ")
+kls = input("Enter your class         : ")
+age = int (input("enter your age      : "))
+hob1 = input("What is your first hobby?       : ")
+hob2 = input("What is your second hobby?      : ")
+hob3 = input("What is your third hobby?       : ")
+hobbies = [hob1, hob2, hob3]
+qut = input("What is your motto?      : ")
+rndg = bool (input("Do you like Rendang? "))
+
+print("\n")
+
+# Data that will show after input section
+print("                     --Your Biodata--")
+print("Name          : ", nama)
+print("Study program : ", prod)
+print("Class         : ", kls)
+print("Age           : ", age)
+print("hobbies       : ", hobbies)
+print("Motto         : ", qut)
+print("Do you like Rendang? : ", rndg)
