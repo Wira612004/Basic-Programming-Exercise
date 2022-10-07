1df32ef87f4254fce0153c70c9287c438b4286d2
Wira612004 <114727898+Wira612004@users.noreply.github.com>
2022-10-07 15:46:49 +0700

    Biodata non-input

---
+++ session3.py
@@ -0,0 +1,17 @@
+# This is my Biodata Perform by Python programming language
+
+# Coding session
+Name = "Wira Harsa Abiyasa Rifki Ghani"
+Major = "Informatics Engineering"
+Class = "TI22I"
+Hobby = "Playing Basketball, Cooking"
+Food  = "Rendang"
+Motto = "We grow through what We go through"
+
+# Display Session
+print("My name is " ,Name)
+print("I am Majoring " ,Major)
+print("I'm in class " ,Class)
+print("My hobby are ", Hobby)
+print("My favourite food is " ,Food)
+print("My motto : " ,Motto)
